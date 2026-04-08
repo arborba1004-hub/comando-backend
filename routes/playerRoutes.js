64e8119f5a1579b64f30eea0eb15d92f2fd0c396
arborba1004-hub <arborba1004@gmail.com>
2026-04-08 17:55:24 -0300

    Atualizar o playerRoutes.js

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -1,1 +1,10 @@
+import { Router } from 'express';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { getMe, updateMe } from '../controllers/playerController.js';
 
+const router = Router();
+
+router.get('/me', authMiddleware, getMe);
+router.patch('/update', authMiddleware, updateMe);
+
+export default router;

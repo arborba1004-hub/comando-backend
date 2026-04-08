4d6f4dfaae040256ff0b73a2f18599cc17eaa74d
arborba1004-hub <arborba1004@gmail.com>
2026-04-08 17:53:38 -0300

    Atualizar o deepMerge.js

--- utils/deepMerge.js
+++ utils/deepMerge.js
@@ -1,1 +1,20 @@
+function isPlainObject(value) {
+  return value !== null && typeof value === 'object' && !Array.isArray(value);
+}
 
+export function deepMerge(target, source) {
+  const output = { ...target };
+
+  for (const key of Object.keys(source)) {
+    const sourceValue = source[key];
+    const targetValue = output[key];
+
+    if (isPlainObject(sourceValue) && isPlainObject(targetValue)) {
+      output[key] = deepMerge(targetValue, sourceValue);
+    } else {
+      output[key] = sourceValue;
+    }
+  }
+
+  return output;
+}

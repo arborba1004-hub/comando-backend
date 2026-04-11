576b113731b28d293a9184a4be306a3c189199a8
arborba1004-hub <arborba1004@gmail.com>
2026-04-11 02:27:31 -0300

    Atualizar o attackRoutes.js

--- routes/attackRoutes.js
+++ routes/attackRoutes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { initiateAttack } from '../controllers/attackController.js';
+import {
+  startBattle,
+  resolveBattle,
+  getBattleReport,
+  getBattleHistory,
+  initiateAttack,
+} from '../controllers/attackController.js';
 
 const router = Router();
 
+router.post('/start', authMiddleware, startBattle);
+router.post('/resolve/:battleId', authMiddleware, resolveBattle);
+router.get('/report/:battleId', authMiddleware, getBattleReport);
+router.get('/history', authMiddleware, getBattleHistory);
+
+// compatibilidade com rota antiga
 router.post('/initiate', authMiddleware, initiateAttack);
 
 export default router;

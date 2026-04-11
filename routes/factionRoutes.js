282cc0847d301e3e34ed1c2e8e7498640cac14f4
arborba1004-hub <arborba1004@gmail.com>
2026-04-11 20:57:34 -0300

    Atualizar o factionRoutes.js

--- routes/factionRoutes.js
+++ routes/factionRoutes.js
@@ -3,13 +3,21 @@ import authMiddleware from '../middlewares/authMiddleware.js';
 import {
   createFaction,
   getMyFaction,
+  listFactions,
   joinFaction,
+  leaveFaction,
+  kickMember,
+  transferLeadership,
 } from '../controllers/factionController.js';
 
 const router = Router();
 
 router.post('/create', authMiddleware, createFaction);
 router.get('/my', authMiddleware, getMyFaction);
+router.get('/list', authMiddleware, listFactions);
 router.post('/join', authMiddleware, joinFaction);
+router.post('/leave', authMiddleware, leaveFaction);
+router.post('/kick', authMiddleware, kickMember);
+router.post('/transfer-leadership', authMiddleware, transferLeadership);
 
 export default router;

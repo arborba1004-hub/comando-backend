ecb0dfafacce2a2a06ad8518c89dcfba896863f1
arborba1004-hub <arborba1004@gmail.com>
2026-04-11 23:17:56 -0300

    Atualizar o factionRoutes.js

--- routes/factionRoutes.js
+++ routes/factionRoutes.js
@@ -1,22 +1,33 @@
 import { Router } from 'express';
-import authMiddleware from '../middlewares/authMiddleware.js';
 import {
   createFaction,
   getMyFaction,
   listFactions,
   joinFaction,
   leaveFaction,
+  donate,
+  invest,
+  updateSettings,
+  updateMemberRole,
   kickMember,
   transferLeadership,
 } from '../controllers/factionController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
-router.post('/create', authMiddleware, createFaction);
-router.get('/my', authMiddleware, getMyFaction);
 router.get('/list', authMiddleware, listFactions);
+router.get('/my', authMiddleware, getMyFaction);
+
+router.post('/create', authMiddleware, createFaction);
 router.post('/join', authMiddleware, joinFaction);
 router.post('/leave', authMiddleware, leaveFaction);
+
+router.post('/donate', authMiddleware, donate);
+router.post('/invest', authMiddleware, invest);
+
+router.post('/update-settings', authMiddleware, updateSettings);
+router.post('/update-member-role', authMiddleware, updateMemberRole);
 router.post('/kick', authMiddleware, kickMember);
 router.post('/transfer-leadership', authMiddleware, transferLeadership);
 

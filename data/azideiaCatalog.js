02ae6405a0583feeb4fe49030c89c3e6aa2ebcb5
arborba1004-hub <arborba1004@gmail.com>
2026-05-21 02:11:11 -0300

    Add files via upload

---
+++ data/azideiaCatalog.js
@@ -0,0 +1,11 @@
+export const AZIDEIA_X9 = {
+  type: 'x9',
+  name: 'X9',
+  modelUrl: 'https://static.wixstatic.com/3d/50f4bf_d07bb1c9327e4f0aaec0681e47314a9e.glb',
+  iconUrl: 'https://static.wixstatic.com/media/50f4bf_ce2c97a1cf324091851178166ed02d29~mv2.png',
+  costDirtyMoney: 5000,
+  activeCount: 10,
+  dailyLimitPerPlayer: 20,
+  rewardType: 'convoy_2x',
+  rewardQuantity: 1,
+};

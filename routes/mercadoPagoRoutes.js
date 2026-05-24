3794f523921859e468f2a440eacccc418d7156ec
arborba1004-hub <arborba1004@gmail.com>
2026-05-24 15:33:49 -0300

    Add files via upload

--- routes/mercadoPagoRoutes.js
+++ routes/mercadoPagoRoutes.js
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { createConvoyCheckout, createCorreCheckout, getPurchaseStatus } from '../controllers/mercadoPagoCheckoutController.js';
+import { createConvoyCheckout, getPurchaseStatus } from '../controllers/mercadoPagoCheckoutController.js';
 import { handleMercadoPagoWebhook } from '../controllers/mercadoPagoWebhookController.js';
-import { getMercadoPagoBrickConfig, createConvoyBrickPayment } from '../controllers/mercadoPagoBrickController.js';
+import {
+  getMercadoPagoBrickConfig,
+  createConvoyBrickPayment,
+  createCorrePackageBrickPayment,
+} from '../controllers/mercadoPagoBrickController.js';
 
 const router = Router();
 
 router.post('/checkout/convoy', authMiddleware, createConvoyCheckout);
-router.post('/checkout/corre', authMiddleware, createCorreCheckout);
 router.get('/brick/config', authMiddleware, getMercadoPagoBrickConfig);
 router.post('/brick/convoy', authMiddleware, createConvoyBrickPayment);
+router.post('/brick/corre-package', authMiddleware, createCorrePackageBrickPayment);
 router.get('/purchases/:purchaseId', authMiddleware, getPurchaseStatus);
 router.post('/webhooks/mercadopago', handleMercadoPagoWebhook);
 

826d3cb7a573f5ec5b91ecb09e585e0a8d5f5ba8
arborba1004-hub <arborba1004@gmail.com>
2026-05-26 22:27:14 -0300

    Add files via upload

--- models/AzideiaRewardBatch.js
+++ models/AzideiaRewardBatch.js
@@ -7,6 +7,7 @@ const azideiaRewardBatchSchema = new mongoose.Schema(
     quantityPerMember: { type: Number, default: 1, min: 1 },
     memberIds: { type: [String], default: [], index: true },
     claimedBy: { type: [String], default: [] },
+    sourceMissionId: { type: String, default: '', index: true },
     sourceTargetType: { type: String, enum: ['x9', 'correria', 'mestre_obras'], default: 'x9' },
     sourceTargetId: { type: String, default: '' },
     killerId: { type: String, required: true, index: true },
@@ -18,5 +19,7 @@ const azideiaRewardBatchSchema = new mongoose.Schema(
 
 azideiaRewardBatchSchema.index({ factionId: 1, rewardType: 1, createdAt: -1 });
 azideiaRewardBatchSchema.index({ factionId: 1, memberIds: 1, claimedBy: 1 });
+azideiaRewardBatchSchema.index({ sourceMissionId: 1, rewardType: 1 }, { sparse: true });
+azideiaRewardBatchSchema.index({ sourceTargetType: 1, sourceTargetId: 1, killerId: 1, rewardType: 1 });
 
 export default mongoose.models.AzideiaRewardBatch || mongoose.model('AzideiaRewardBatch', azideiaRewardBatchSchema);

b17e787a7e73c99eb25b91238622954200663213
arborba1004-hub <arborba1004@gmail.com>
2026-05-30 01:44:11 -0300

    Add files via upload

--- routes/fugaRoutes.js
+++ routes/fugaRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { buyFugaVehicle } from '../controllers/fugaController.js';
+import {
+  buyFugaCatalogAccessory,
+  buyFugaVehicle,
+  buyFugaVehicleUpgrade,
+} from '../controllers/fugaController.js';
 
 const router = Router();
 
 router.post('/buy', authMiddleware, buyFugaVehicle);
+router.post('/accessory/buy', authMiddleware, buyFugaCatalogAccessory);
+router.post('/vehicle-upgrade/buy', authMiddleware, buyFugaVehicleUpgrade);
 
 export default router;
